a573bee59c455c35d9d1c27f1b395008f8a37b6b
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-04-29 19:23:55 +0600

    Some bugs are fixed and resposive layout created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,13 @@
-const displayTime = document.querySelector(".time");
+const timeText = document.querySelector(".time h2");
 const displayDate = document.querySelector(".date p");
 
-let timeText = document.createElement("h2");
-displayTime.appendChild(timeText);
-function updateTime() {
-  let currentTime = new Date();
-  currentTime = currentTime.toLocaleTimeString();
-  timeText.innerText = currentTime;
-}
-
-function updateDate() {
-  let currentDate = new Date();
-  currentDate = currentDate.toDateString();
-  displayDate.innerText = currentDate;
-}
-
-function startUpdatingTime() {
-  updateTime();
-  setInterval(updateTime, 1000);
+function updateTimeAndDate() {
+  const now = new Date();
+  timeText.textContent = now.toLocaleTimeString();
+  displayDate.textContent = now.toDateString();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  updateDate();
-  startUpdatingTime();
+  updateTimeAndDate();
+  setInterval(updateTimeAndDate, 1000);
 });
